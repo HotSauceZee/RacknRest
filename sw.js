7230e8f6233e8078a03fa69cf6e2902486829458
Your Name <you@example.com>
2026-01-08 13:13:35 -0600

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plate-timer-v4';
+const CACHE_NAME = 'plate-timer-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -7,10 +7,24 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force this new SW to verify and install immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(), // Take control of all clients immediately
+      caches.keys().then((keys) => { // Delete all old caches
+        return Promise.all(
+          keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        );
+      })
+    ])
   );
 });
 
